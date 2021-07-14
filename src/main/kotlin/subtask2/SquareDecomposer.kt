4030ffedfcf806772859a01ad30f2bb574e27c9b
OrLove101 <danyazerorebel@gmail.com>
2021-07-14 21:19:38 +0300

    docs: third task solved

--- src/main/kotlin/subtask2/SquareDecomposer.kt
+++ src/main/kotlin/subtask2/SquareDecomposer.kt
@@ -1,20 +1,34 @@
 package subtask2
 
+import kotlin.math.sqrt
+
 class SquareDecomposer {
 
     fun decomposeNumber(number: Int): Array<Int>? {
-        val result = mutableListOf<Int>()
-        var resultSquareSum: Int = 0
-        var tempNumber: Int = number
-
-        val squareNumber: Int = number*number
-
-        while ( resultSquareSum != squareNumber ) {
-            tempNumber--
-            if ( resultSquareSum+tempNumber*tempNumber <= squareNumber ) {
-                result.add(tempNumber*tempNumber)
-            }
+        if ( number < 5 ) {
+            return null
         }
-        return result.asReversed().toTypedArray()
+
+        val squareNumber = number * number
+
+        return decomposer(squareNumber, number-1)
+    }
+
+    private fun decomposer(squareNumber: Int, prevDigit: Int): Array<Int>? {
+        for ( decomposedDigit in prevDigit downTo 1 ) {
+            val numResult = squareNumber - decomposedDigit * decomposedDigit
+
+            if ( numResult == 0 ) return arrayOf(decomposedDigit)
+            else if ( numResult < 0 ) return null
+
+            val nextNumber = sqrt(numResult.toDouble()).toInt()
+            val result = if ( nextNumber >= decomposedDigit ) {
+                decomposer(numResult, decomposedDigit - 1)
+            } else {
+                decomposer(numResult, nextNumber)
+            }
+            if (result != null) return result + arrayOf(decomposedDigit)
+        }
+        return null
     }
 }
